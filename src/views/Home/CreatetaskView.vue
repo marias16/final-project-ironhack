8a8714d3045cdca0099ac310f00aac7a9ad98a14
Maria Suarez <msuarezh@gmail.com>
2023-05-10 19:39:41 +0200

    create task  view on mobile added validation

--- src/views/Home/CreatetaskView.vue
+++ src/views/Home/CreatetaskView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { computed } from 'vue'
 import router from '@/router/index'
 import { useTasksStore } from '@/stores/tasks'
 import { useUsersStore } from '@/stores/users'
@@ -6,18 +7,23 @@ import { useUsersStore } from '@/stores/users'
 const tasks = useTasksStore();
 const users = useUsersStore()
 
+tasks.titleTask = ''
+
 async function addAndGo () {
     await tasks._addNewTask({title: tasks.titleTask, user_id: users.currentUser.id})
     await router.push({name: 'home'})
 }
 
+const emptyField = computed(() => {
+    return tasks.titleTask === '' ? true : false; 
+})
 </script>
 
 <template>
     <h2>Create your task.</h2>
-    <input type="text" class="input input-bordered" v-model="tasks.titleTask" placeholder="Ex. Do the dishes"> 
+    <input type="text" class="input input-bordered" v-model="tasks.titleTask" placeholder="Ex. Walk the dog"> 
     <section>
-        <button class="btn btn-accent" @click="addAndGo">ADD</button>
+        <button class="btn btn-accent" :class="{'btn-disabled': emptyField}" @click="addAndGo">ADD</button>
         <button class="btn btn-ghost" @click="router.push({name: 'home'})">Back</button>
     </section>
     
